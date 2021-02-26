40c9adc453777c4d96d2103e67012fa588adfcc9
Noel Sellon <37094946+nzealion@users.noreply.github.com>
2021-02-25 18:06:54 -0600

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-let btnNav = document.querySelector('.btn-nav');
-let mainContent = document.querySelector('.main-content');
-
-btnNav.addEventListener('click', ()=>{
-    btnNav.classList.toggle('toggle');
-    mainContent.classList.toggle('toggle');
-});
